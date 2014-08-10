8b067bbaea665c75575edf5a03a7c4969a6b5897
daniel.koberstein@gmail.com <daniel.koberstein@gmail.com>
2014-08-10 08:05:50 -0700

    Corrected commandline options and added better command line usage text.

--- SyncSharkApp/SyncSharkUI.cs
+++ SyncSharkApp/SyncSharkUI.cs
@@ -18,23 +18,22 @@ namespace SyncSharkApp
         [Option('r', "right", Required = true, HelpText = "Right directory path.")]
         public string RightDirectoryPath { get; set; }
 
-        [Option('s', "sync", Required = false, DefaultValue = false, HelpText = "Syncronizes changes in both folders.")]
-        public bool Mirror { get; set; }
-
-        [Option('m', "mirror", Required = false, DefaultValue = true, HelpText = "Mirrors files from the left directory to the right directory.")]
+        [Option('s', "sync", Required = false, DefaultValue = true, HelpText = "Syncronizes changes in both folders.")]
         public bool Sync { get; set; }
+
+        [Option('m', "mirror", Required = false, DefaultValue = false, HelpText = "Mirrors files from the left directory to the right directory.")]
+        public bool Mirror { get; set; }
 
         [HelpOption]
         public string GetUsage()
         {
             // this without using CommandLine.Text
             var usage = new StringBuilder();
-            usage.AppendLine(@"SyncSharkApp");
-            usage.AppendLine();
-            usage.AppendLine(@"  \l or \left - Left directory path");
-            usage.AppendLine(@"  \r or \right - Right directory path");
-            usage.AppendLine(@"  \s or \sync - Syncronizes changes in both folders");
-            usage.AppendLine(@"  \m or \mirror - Mirrors files from the left directory to the right directory");
+            usage.AppendLine(@"usage: SyncSharkApp [-l <dirPath>] [-r <dirPath>] [-s] [-m]");
+            usage.AppendLine(@"  -l or -left     Specify the left directory path");
+            usage.AppendLine(@"  -r or -right    Specify the right directory path");
+            usage.AppendLine(@"  -s or -sync     Syncronizes changes in both directories");
+            usage.AppendLine(@"  -m or -mirror   Mirrors files from the left directory to the right directory");
             return usage.ToString();
         }
     }
